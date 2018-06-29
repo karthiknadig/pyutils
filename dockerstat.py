771be0fd14721b60c8dd25927e7070779c3c2586
Karthik Nadig <karthik.nadig@live.com>
2018-06-29 00:34:01 -0700

    Few bug fixes

--- dockerstat.py
+++ dockerstat.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import os
 import sys
 import time
 
@@ -14,11 +13,17 @@ __author__ = 'Karthik Nadig'
 __version__ = '1.0.0a'
 
 DOCKER_STAT_OK = 0
+DOCKER_STAT_API = 'https://hub.docker.com/v2/repositories/'
+
+
+def _get_stat_url(repo):
+    return '%s%s' % (DOCKER_STAT_API, repo)
+
 
 def _parse_args():
     prog = sys.argv[0]
     usage = """
-     %s [-h|--help] [-V|--version] --repo REPO --file FILENAME
+     %s [-h|--help] [-V|--version] [--file FILENAME] --repo REPO
     """ % prog
     parser = argparse.ArgumentParser(
         prog=prog,
@@ -26,31 +31,37 @@ def _parse_args():
     )
 
     parser.add_argument('--repo', type=str, required=True)
-    parser.add_argument('--filename', type=str, required=True)
+    parser.add_argument('--filename', type=str)
     parser.add_argument('-V', '--version', action='version')
     parser.version = __version__
     return vars(parser.parse_args(sys.argv[1:]))
 
 
 def process_command():
+
     args = _parse_args()
 
-    req = url_lib.urlopen('https://hub.docker.com/v2/repositories/%s' % args['repo'])
-    data = json.loads(req.read())
+    req = url_lib.urlopen(_get_stat_url(args['repo']))
+    resp = req.read()
+    if not isinstance(resp, str):
+        resp = resp.decode('utf-8')
+    data = json.loads(resp)
     msg = '%s, \"%s\", %s\n' % (str(time.time()),
-                              time.asctime(),
-                              str(data['pull_count']))
+                                time.asctime(),
+                                str(data['pull_count']))
 
-    filename = args['filename']
-    if filename == 'stdout':
+    try:
+        filename = args['filename']
+        if filename is None:
+            print(msg)
+        else:
+            with open(filename, 'a') as fp:
+                fp.write(msg)
+    except KeyError:
         print(msg)
-    else:
-        with open(filename, 'a') as fp:
-            fp.write(msg)
 
     return DOCKER_STAT_OK
 
 
 if __name__ == '__main__':
     process_command()
-
